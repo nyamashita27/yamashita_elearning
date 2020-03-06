189f23ac4775689a1bd0c933ad814784371d3741
Nanako.Y <nanako@MacBook-Pro.local>
2020-03-06 09:41:48 +0800

    Fixed login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
   before_action :check_signed_in, only: [:new]
-  before_action :check_admin_user, only: [:admin]
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
@@ -21,8 +20,8 @@ class SessionsController < ApplicationController
 
   private
 
-  def check_signed_in
-    redirect_to(root_url) if logged_in?
-  end
+    def check_signed_in
+      redirect_to(root_url) if logged_in?
+    end
 
 end
