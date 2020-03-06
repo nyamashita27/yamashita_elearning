a9d08b92b07a72faae62788803f484c2a9db7591
nyamashita27 <61719292+nyamashita27@users.noreply.github.com>
2020-03-06 12:16:38 +0800

    Finished edit-user (#5)
    * Finished edit-user
    
    * fixed edit-user
    
    Co-authored-by: Nanako.Y <nanako@MacBook-Pro.local>

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, allow_nil: true #編集時にPWなくても更新できる
   has_secure_password
+  mount_uploader :avatar, AvatarUploader
 end
