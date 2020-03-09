8d9f8ef49d4951a6b31e50857465d37edc80bbf3
Nanako.Y <nanako@MacBook-Pro.local>
2020-03-09 11:50:49 +0800

    fixed show-page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,9 +2,9 @@ module UsersHelper
   def user_avatar(user, classes)
     @avatar = user.avatar
     if @avatar.present?
-      @avatar_user = image_tag(@avatar.url, class: "classes")
+      @avatar_user = image_tag(@avatar.url, class: classes)
     else
-      @avatar_user = image_tag("default.png", class: "classes")
+      @avatar_user = image_tag("default.png", class: classes)
     end
   end
 end
