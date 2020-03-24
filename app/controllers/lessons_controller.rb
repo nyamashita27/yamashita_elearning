35a25fbb0658768361de9bf9a9e28c8f0ac5ec9c
Nanako.Y <nanako@MacBook-Pro.local>
2020-03-24 11:48:31 +0800

    added before_action to the lesson-answering

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,7 @@
 class LessonsController < ApplicationController
 
+  before_action :logged_in_user
+  
   def create
     @category = Category.find(params[:category_id])
     @lesson = @category.lessons.build(user: current_user) #lesson modelにuserと一緒に記録
