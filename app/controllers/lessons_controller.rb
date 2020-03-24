7196c5b8be053a9ad56b366ccaf97ea7bdd8cb88
nyamashita27 <61719292+nyamashita27@users.noreply.github.com>
2020-03-24 12:58:48 +0800

    finished the lesson-answering (#19)
    * finished the lesson-answering
    
    * added before_action to the lesson-answering
    
    Co-authored-by: Nanako.Y <nanako@MacBook-Pro.local>

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,7 @@
 class LessonsController < ApplicationController
 
+  before_action :logged_in_user
+  
   def create
     @category = Category.find(params[:category_id])
     @lesson = @category.lessons.build(user: current_user) #lesson modelにuserと一緒に記録
@@ -7,6 +9,11 @@ class LessonsController < ApplicationController
     if @lesson.save
       redirect_to new_lesson_answer_url(@lesson)
     end
+  end
+
+  def show
+    @lesson = Lesson.find(params[:id])
+    @answers = @lesson.answers.paginate(page: params[:page], per_page: 5).order(updated_at: :desc)
   end
 
 end
